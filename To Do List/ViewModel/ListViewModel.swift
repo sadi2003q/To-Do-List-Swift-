56f098e2a63b458665d87eb883b313a75862e1dc
Md. Adnan Abdullah <Adnanabdullah625@gmail.com>
2025-01-11 13:21:43 +0600

    Unnecessary Comment Deleted

--- To Do List/ViewModel/ListViewModel.swift
+++ To Do List/ViewModel/ListViewModel.swift
@@ -8,56 +8,6 @@
 import Foundation
 import SwiftData
 
-/*
- @Observable
- class ListViewModel {
- 
- var items: [ItemModel] = []
- 
- 
- init () {
- getItems()
- }
- 
- 
- func getItems () {
- let newItem : [ItemModel] = [
- ItemModel(title:  "this is the first title", isComplete: true),
- ItemModel(title: "this is the second title", isComplete: false),
- ItemModel(title: "this is the third title", isComplete: true)
- ]
- 
- items.append(contentsOf: newItem)
- }
- 
- func onDelete(indexSet: IndexSet) {
- items.remove(atOffsets: indexSet)
- }
- 
- func onMove(indexSet: IndexSet, newOffset: Int) {
- items.move(fromOffsets: indexSet, toOffset: newOffset)
- }
- 
- func addItem(title: String) {
- let item = ItemModel(title: title, isComplete: false)
- items.append(item)
- }
- 
- func updateItem(item: ItemModel) {
- //        if let index = items.firstIndex { currentItem in
- //            return currentItem.id == item.id
- //        }
- 
- if let index = items.firstIndex(where: {$0.id == item.id}){
- items[index] = item.updateCompletion()
- 
- }
- }
- 
- 
- }
- */
-
 @Observable
 class ListViewModel {
     var items: [ItemModel] = []
